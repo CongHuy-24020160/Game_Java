a3aa19c3d7d5ca58925404a0a3d23a5d390854ec
NTDO <thanhdo050@gmail.com>
2025-10-07 09:33:44 +0700

    Cập nhật BallComponent, LinkedEntityComponent, PhysicsBodyComponent

---
+++ code here/LinkedEntityComponent.java
@@ -0,0 +1,13 @@
+
+// import packback vô đây sau khi code 
+
+import com.badlogic.ashley.core.Component;
+import com.badlogic.ashley.core.Entity;
+// cái này sẽ cho phép 1 thực thể sẽ gắn với 1 thực thể khác
+public class LinkedEntityComponent {
+    public Entity LinkedEntity;
+    public void setLinkedEntity(Entity entity)  {
+        System.out.println(entity);
+        this.LinkedEntity = entity;
+    }
+}
