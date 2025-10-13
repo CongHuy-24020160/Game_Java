35927565a79a2d47c07811984e2efbea71117aa1
CongHuy-24020160 <24020160@vnu.edu.vn>
2025-10-13 09:16:32 +0700

    Fix errors next

--- core/src/game/component/TypeComponent.java
+++ core/src/game/component/TypeComponent.java
@@ -1,12 +1,13 @@
 package game.component;// import package vô đây
 
-import com.badlogic.ashley.core.component;
+import com.badlogic.ashley.core.Component;
+import com.badlogic.gdx.utils.Pool;
 
 /**
  * hàm để xác định loại của thực thể
  */
 
-public class TypeComponent {
+public class TypeComponent implements Component, Pool.Poolable {
     public static final int PLAYER_TYPE = 1 ;
     public static final int BALL_TYPE = 2;
     public static final int BLOCK_TYPE = 3;
